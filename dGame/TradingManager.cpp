89250f0e380858147298f1165cd8bb9347d89b8f
RediPanda <redi@nekonii.xyz>
2022-01-05 14:16:02 +1100

    Update TradingManager.cpp

--- dGame/TradingManager.cpp
+++ dGame/TradingManager.cpp
@@ -238,13 +238,13 @@ void Trade::Complete()
     // Append data to DB table.
     std::cout << printerValue;
 
-    for (const auto i: m_ItemsA) {
-      std::cout << i << ' ';
+    for (const auto& i: m_ItemsA) {
+      std::cout << i.itemLot << ' ';
       std::cout << "\n";
     }
 
-    for (const auto i: m_ItemsB) {
-      std::cout << i << ' ';
+    for (const auto& i: m_ItemsB) {
+      std::cout << i.itemLot << ' ';
       std::cout << "\n";
     }
 
